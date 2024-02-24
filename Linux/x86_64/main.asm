0aca4f31f4ab42406fead3b43b7d9b62870e591d
ragingo <save.the.world.crush@gmail.com>
2024-02-24 23:51:56 +0900

    [Linux/x86_64] byteswap16 の動作確認コード追加

--- Linux/x86_64/main.asm
+++ Linux/x86_64/main.asm
@@ -20,5 +20,44 @@ _start:
     call sample005_kuku
     call sample006_file_write
 
+    call temp_samples
+
     mov rdi, rax
     call sys_exit
+
+; ==========
+
+%include "lib/include/ascii.inc"
+%include "lib/include/bit.inc"
+
+temp_samples:
+    call temp_sample1
+    ret
+
+section .text
+    extern byteswap16
+    extern byteswap32
+    extern byteswap64
+    extern print_number
+    extern putchar
+
+temp_sample1:
+    mov rdi, MAKE_WORD('A', 'B')
+    call byteswap16
+    cmp rax, MAKE_WORD('A', 'B')
+    je .temp_sample1.failure
+    cmp rax, MAKE_WORD('B', 'A')
+    je .temp_sample1.success
+.temp_sample1.failure:
+    mov rdi, 0 ; false
+    mov rsi, 0
+    call print_number ; MEMO: 16進で出力できるようにしたい
+    jmp .temp_sample1.finish
+.temp_sample1.success:
+    mov rdi, 1 ; true
+    mov rsi, 0
+    call print_number
+.temp_sample1.finish:
+    mov rdi, ASCII_LF
+    call putchar
+    ret
