7af5171b49f9fb421c0285d0dbaefb8670c43f95
ragingo <save.the.world.crush@gmail.com>
2024-02-28 22:25:41 +0900

    [Linux/x86_64] ビットマップ画像生成コードのリファクタ

--- Linux/x86_64/sample/007_bmp.asm
+++ Linux/x86_64/sample/007_bmp.asm
@@ -8,6 +8,18 @@ section .text
     extern fopen
     extern sys_write
 
+%define BI_RGB 0
+%define BITMAPFILEHEADER_SIZE 14
+%define BITMAPINFOHEADER_SIZE 40
+%define BITMAP_HEADER_SIZE (BITMAPFILEHEADER_SIZE + BITMAPINFOHEADER_SIZE)
+%define BITMAP_PIXEL_OFFSET BITMAP_HEADER_SIZE
+%define BITMAP_WIDTH 32
+%define BITMAP_HEIGHT 32
+%define BITMAP_BITS_PER_PIXEL 24
+%define BITMAP_DATA_SIZE (BITMAP_WIDTH * BITMAP_HEIGHT * BITMAP_BITS_PER_PIXEL / 8)
+%define BITMAP_FILE_SIZE (BITMAP_HEADER_SIZE + BITMAP_DATA_SIZE)
+%define BITMAP_ROW_STRIDE (BITMAP_WIDTH * BITMAP_BITS_PER_PIXEL / 8)
+
 sample007_bmp:
     push rbp
     mov rbp, rsp
@@ -19,7 +31,7 @@ sample007_bmp:
     pop r8 ; 破棄
     mov r8, rax ; fd
 
-    sub rsp, 1024 * 3 + 54
+    sub rsp, BITMAP_FILE_SIZE
     mov rdi, rsp
     call create_bitmap
 
@@ -27,7 +39,7 @@ sample007_bmp:
 
     mov rdi, r8 ; fd
     mov rsi, r9 ; buf
-    mov rdx, 1024 * 3 + 54
+    mov rdx, BITMAP_FILE_SIZE
     call sys_write
 
     leave
@@ -39,37 +51,34 @@ create_bitmap:
     mov rbp, rsp
 
     ; file header
-    mov word [rdi + 0], MAKE_WORD('B', 'M')
-    mov dword [rdi + 2], 1024 * 3 + 54
-    mov word [rdi + 6], 0
-    mov word [rdi + 8], 0
-    mov dword [rdi + 10], 54
+    ; https://learn.microsoft.com/ja-jp/windows/win32/api/wingdi/ns-wingdi-bitmapfileheader
+    mov word  [rdi +  0], MAKE_WORD('B', 'M')   ; bfType
+    mov dword [rdi +  2], BITMAP_FILE_SIZE      ; bfSize
+    mov word  [rdi +  6], 0                     ; bfReserved1
+    mov word  [rdi +  8], 0                     ; bfReserved2
+    mov dword [rdi + 10], BITMAP_PIXEL_OFFSET   ; bfOffBits
 
     ; info header
-    mov dword [rdi + 14], 40
-    mov dword [rdi + 18], 32
-    mov dword [rdi + 22], 32
-    mov word [rdi + 26], 1
-    mov word [rdi + 28], 24
-    mov dword [rdi + 30], 0
-    mov dword [rdi + 34], 1024 * 3
-    mov dword [rdi + 38], 0
-    mov dword [rdi + 42], 0
-    mov dword [rdi + 46], 0
-    mov dword [rdi + 50], 0
+    ; https://learn.microsoft.com/ja-jp/windows/win32/api/wingdi/ns-wingdi-bitmapinfoheader
+    mov dword [rdi + 14], BITMAPINFOHEADER_SIZE ; biSize
+    mov dword [rdi + 18], BITMAP_WIDTH          ; biWidth
+    mov dword [rdi + 22], BITMAP_HEIGHT         ; biHeight
+    mov word  [rdi + 26], 1                     ; biPlanes
+    mov word  [rdi + 28], BITMAP_BITS_PER_PIXEL ; biBitCount
+    mov dword [rdi + 30], BI_RGB                ; biCompression
+    mov dword [rdi + 34], BITMAP_DATA_SIZE      ; biSizeImage
+    mov dword [rdi + 38], 0                     ; biXPelsPerMeter
+    mov dword [rdi + 42], 0                     ; biYPelsPerMeter
+    mov dword [rdi + 46], 0                     ; biClrUsed
+    mov dword [rdi + 50], 0                     ; biClrImportant
 
     ; pixels
+    push rdi
+    lea rdi, [rdi + BITMAP_PIXEL_OFFSET]
     call create_bitmap_data
-    ; call create_bitmap_data_2
+    pop rdi
 
     leave
-    ret
-
-create_bitmap_data_2:
-    mov rcx, 1024 * 3
-.create_bitmap_data_2.loop:
-    mov byte [rdi + 54 + rcx], cl
-    loop .create_bitmap_data_2.loop
     ret
 
 create_bitmap_data:
@@ -89,25 +98,26 @@ create_bitmap_data:
 
     ; row * stride + col
     mov rax, r8
-    imul rax, 32 * 3
+    imul rax, BITMAP_ROW_STRIDE
     add rax, r9
 
-    mov byte [rdi + 54 + rax + 0], al
-    mov byte [rdi + 54 + rax + 1], al
-    mov byte [rdi + 54 + rax + 2], al
+    mov byte [rdi + rax + 0], al
+    mov byte [rdi + rax + 1], al
+    mov byte [rdi + rax + 2], al
 
     pop r9
     pop r8
 
     add r9, 3
-    cmp r9, 32 * 3
+    cmp r9, BITMAP_ROW_STRIDE
     jl .create_bitmap_data.loop.col
 
     inc r8
-    cmp r8, 32
+    cmp r8, BITMAP_HEIGHT
     jl .create_bitmap_data.loop.row
 
     pop r9
     pop r8
+
     leave
     ret
